b8399a540cb883e13765f7858c55635afec2a6c6
Russell Keith-Magee <russell@keith-magee.com>
2014-08-16 13:12:35 +0800

    Initial commit of code after repository split.

---
+++ setup.py
@@ -0,0 +1,48 @@
+#/usr/bin/env python
+import io
+import re
+from setuptools import setup, find_packages
+
+
+with io.open('./toga_cassowary/__init__.py', encoding='utf8') as version_file:
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
+    if version_match:
+        version = version_match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+with io.open('README.rst', encoding='utf8') as readme:
+    long_description = readme.read()
+
+
+setup(
+    name='toga-cassowary',
+    version=version,
+    description='A helper layer for the Toga widget toolkit for backends using the Cassowary contraint algorithm for layout.',
+    long_description=long_description,
+    author='Russell Keith-Magee',
+    author_email='russell@keith-magee.com',
+    url='http://pybee.org/toga',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'cassowary',
+    ],
+    license='New BSD',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Software Development :: Widget Sets',
+    ],
+    test_suite='tests',
+)
